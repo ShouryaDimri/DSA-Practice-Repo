81e29e7ddfb8aab0bcbfb3567798ffef8898e16c
Shourya Dimri <shouryadimricareer@gmail.com>
2026-07-16 02:21:27 +0530

    Solved Selection Sort - takeUforward

---
+++ selection-sort/selection-sort.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> selectionSort(vector<int>& 
+    nums) {
+        int n = nums.size();
+        for (int i=0; i<n-1; i++){
+            int min = i;
+            for (int j= i+1; j< n; j++){
+                if (nums[j] < nums[min]){
+                    min = j;
+                }
+            }
+            if (min != i){
