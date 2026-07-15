67c7fb74a8057143d2f4fb8171492e767d4cc869
Shourya Dimri <shouryadimricareer@gmail.com>
2026-07-16 02:32:01 +0530

    Solved Bubble Sort - Runtime: 0.2s, Memory: N/A - takeUforward

---
+++ bubble-sort/bubble-sort.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> bubbleSort(vector<int>& nums) {
+        int n = nums.size();
+        for (int i =n-1; i>=0; i--){
+            bool swapped = false;
+            for (int j=0; j<i ; j++){
+                if(nums[j]> nums[j+1]){
+                    swap(nums[j], nums[j+1]);
+                    swapped = true;
+                }
+            }
