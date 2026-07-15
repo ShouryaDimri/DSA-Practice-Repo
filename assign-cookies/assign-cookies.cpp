3374fdd334998fd251602ff38460d7a86a95c985
Shourya Dimri <shouryadimricareer@gmail.com>
2026-07-16 02:16:23 +0530

    Solved Assign Cookies - takeUforward

---
+++ assign-cookies/assign-cookies.cpp
@@ -0,0 +1,12 @@
+class Solution{    
+    public:
+    int findMaximumCookieStudents(vector<int>& Student, vector<int>& Cookie)
+    {
+        //your code goes here
+        int m= Cookie.size();
+        int n= Student.size();
+        sort(Cookie.begin(), Cookie.end());
+        sort(Student.begin(), Student.end());
+        int l=0, r=0;
+        while(l<n && r <m){
+            if(Cookie[r]>= Student[l])l++;
