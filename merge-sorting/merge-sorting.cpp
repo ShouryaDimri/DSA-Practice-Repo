a053f1e9a44d99779873edf30fde8b29234ff861
Shourya Dimri <shouryadimricareer@gmail.com>
2026-07-16 03:15:24 +0530

    Solved Merge Sorting - Runtime: 0.2s, Memory: N/A - takeUforward

--- merge-sorting/merge-sorting.cpp
+++ merge-sorting/merge-sorting.cpp
@@ -1,12 +1,13 @@
 class Solution{
 public:
-    // Function to merge two sorted halves of the array
-    void merge(vector<int> &arr, int low, int mid, int high) {
-        // Temporary array to store merged elements
+    // Function to merge two sorted halves of 
+    the array
+    void merge(vector<int> &arr, int low, int 
+    mid, int high) {
+        // Temporary array to store merged 
+        elements
         vector<int> temp;
         int left = low;
         int right = mid + 1;
 
         // Loop until subarrays are exhausted
-        while (left <= mid && right <= high) {
-            // Compare left and right elements
