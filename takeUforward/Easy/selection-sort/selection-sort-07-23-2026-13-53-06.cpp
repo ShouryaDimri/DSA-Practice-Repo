f24c8dfba15b73e5d6106215c4bf9fc8b65cf19d
Shourya Dimri <shouryadimricareer@gmail.com>
2026-07-23 13:53:09 +0530

    selection-sort C++ DSA Easy

---
+++ takeUforward/Easy/selection-sort/selection-sort-07-23-2026-13-53-06.cpp
@@ -0,0 +1,13 @@
+        for (int i = 0; i < n - 1; i++){
+            int min = i;
+            for (int j = i+1; j<n ; j++){
+                if(nums[j] < nums[min]){
+                    min = j;
+                }
+            }
+        }
+        return nums;
+    }
+            if(min != i){
+                swap(nums[min], nums[i]);
+            }
