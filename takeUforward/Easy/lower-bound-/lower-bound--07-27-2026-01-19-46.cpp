dbb5c3e99c2b5e90abcc14270fe1112975e35281
Shourya Dimri <shouryadimricareer@gmail.com>
2026-07-27 01:19:47 +0530

    lower-bound- C++ DSA Easy

---
+++ takeUforward/Easy/lower-bound-/lower-bound--07-27-2026-01-19-46.cpp
@@ -0,0 +1,19 @@
+        while (low <= high){
+            int mid = (high + low)/2 ; 
+            if(nums[mid]>= x){
+                ans = mid;
+            }
+                high = mid-1;
+            else{
+                low = mid + 1;
+        int low =0;
+        int high = nums.size()-1;
+        int ans = nums.size();
+class Solution{
+public:
+    int lowerBound(vector<int> &nums, int x){
+            }
+        }return ans;
+    }
+            // x is less than value of mid index
+};
