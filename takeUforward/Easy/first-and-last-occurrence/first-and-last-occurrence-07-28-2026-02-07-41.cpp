8bd1f904c9ba443fd7312e0e17df32c60d4310cb
Shourya Dimri <shouryadimricareer@gmail.com>
2026-07-28 02:07:41 +0530

    first-and-last-occurrence C++ DSA Easy

---
+++ takeUforward/Easy/first-and-last-occurrence/first-and-last-occurrence-07-28-2026-02-07-41.cpp
@@ -0,0 +1,13 @@
+                    first = i;
+                if(first == -1){
+                    
+            if (nums[i]== target){
+        for(int i =0; i< n; i++){
+                }
+                last = i;
+                
+        int last = -1;
+        vector<int> ans;
+    vector<int> searchRange(vector<int> &nums, int target) {
+        int n = nums.size();
+        int first = -1;
