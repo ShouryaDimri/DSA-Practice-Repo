0796f0fcd9b091dca2ef080a3dffbe21040349e9
Shourya Dimri <shouryadimricareer@gmail.com>
2026-07-28 15:59:57 +0530

    count-occurrences-in-a-sorted-array C++ DSA Easy

---
+++ takeUforward/Easy/count-occurrences-in-a-sorted-array/count-occurrences-in-a-sorted-array-07-28-2026-15-59-55.cpp
@@ -0,0 +1,13 @@
+        // Your code goes here
+public:
+    int countOccurrences(vector<int>& arr, int target) {
+class Solution {
+        int count = 0;
+        for (int i=0; i< arr.size(); i++){
+            if(arr[i] == target){
+        }
+                count ++;
+            }
+    }
+        return count ; 
+};
