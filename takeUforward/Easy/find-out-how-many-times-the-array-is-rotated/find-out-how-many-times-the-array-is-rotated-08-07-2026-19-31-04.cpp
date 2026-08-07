e067d23e814196388a8a1aa8115de82ce80b3acd
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-07 19:31:04 +0530

    find-out-how-many-times-the-array-is-rotated C++ DSA Easy

---
+++ takeUforward/Easy/find-out-how-many-times-the-array-is-rotated/find-out-how-many-times-the-array-is-rotated-08-07-2026-19-31-04.cpp
@@ -0,0 +1,13 @@
+      while (low <= high){
+        int mid = (low+high)/2;
+        //check left part sorted
+    int findKRotation(vector<int>& arr)  {
+      int low =0;
+      int n = arr.size();
+      int high = n-1;
+      //temp ans value
+      int ans = arr[0];
+      int index = 0;
+
+class Solution {
+public:
