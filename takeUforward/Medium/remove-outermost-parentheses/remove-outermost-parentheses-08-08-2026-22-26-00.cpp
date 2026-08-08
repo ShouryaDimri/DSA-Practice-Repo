f6b4670e50ab4d9c18907194d899f2c88742c075
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-08 22:26:01 +0530

    remove-outermost-parentheses C++ DSA Medium

---
+++ takeUforward/Medium/remove-outermost-parentheses/remove-outermost-parentheses-08-08-2026-22-26-00.cpp
@@ -0,0 +1,13 @@
+            if (c=='('){
+                if(balance >0){
+        int balance =0;
+        for (char c:s){
+        // Your code goes here
+        string res ="";
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        //this is resultant string adding only if balance greater than 0
+        //parsing through string
+                //balance >0 -> inner depth level 
