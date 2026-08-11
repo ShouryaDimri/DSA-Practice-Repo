506fc58704c4bb6275fd8ef448328b1041a2c3e1
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-12 00:26:17 +0530

    single-element-in-sorted-array C++ DSA Medium

---
+++ takeUforward/Medium/single-element-in-sorted-array/single-element-in-sorted-array-08-12-2026-00-26-16.cpp
@@ -0,0 +1,13 @@
+            //pair(1st 2nd)= (even, odd)
+            //value: 1 1 2 3 3 4 4
+            //index: 0 1 2 3 4 5 6
+            //making mid even
+            //common pattern 
+
+        while(low<high){
+        int low =0;
+        int n= nums.size();
+        int high = n-1;
+public:
+    int singleNonDuplicate(vector<int> &nums) {
+class Solution {
