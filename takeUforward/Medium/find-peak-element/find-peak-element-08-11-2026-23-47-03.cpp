a0c01e75a2567d03c8233b8237cb32c9e39d8103
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-11 23:47:04 +0530

    find-peak-element C++ DSA Medium

---
+++ takeUforward/Medium/find-peak-element/find-peak-element-08-11-2026-23-47-03.cpp
@@ -0,0 +1,13 @@
+
+            if(nums[mid]> nums[mid+1]){
+                high =mid;
+            }
+
+        while(low <high){
+            int mid = (low + high)/2;
+        int low = 0;
+        int n = nums.size();
+        int high = n-1;
+public:
+    int findPeakElement(vector<int>& nums) {
+class Solution {
