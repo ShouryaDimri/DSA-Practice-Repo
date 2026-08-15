2462b379a8d4a4d5091eb8f91915f281f9b632e8
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-16 02:54:47 +0530

    find-the-length-of-the-linked-list C++ DSA Easy

---
+++ takeUforward/Easy/find-the-length-of-the-linked-list/find-the-length-of-the-linked-list-08-16-2026-02-54-47.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getLength(ListNode* head) {
+        // Your code goes here
+        int count =0;
+        while(head != nullptr){
+            count ++;
+        }
+    }
+            head= head->next;
+        return count;
+};
