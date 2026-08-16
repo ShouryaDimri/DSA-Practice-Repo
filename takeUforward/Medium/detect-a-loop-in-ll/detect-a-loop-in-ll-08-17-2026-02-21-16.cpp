1ec19583fc2e345f833868194f3cb62d5422931b
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-17 02:21:17 +0530

    detect-a-loop-in-ll C++ DSA Medium

---
+++ takeUforward/Medium/detect-a-loop-in-ll/detect-a-loop-in-ll-08-17-2026-02-21-16.cpp
@@ -0,0 +1,12 @@
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
