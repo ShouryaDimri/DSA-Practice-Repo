128c20d1e15c4f71909d813c022038bde3c74df2
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-18 01:39:03 +0530

    valid-anagram C++ DSA Easy

---
+++ takeUforward/Easy/valid-anagram/valid-anagram-08-18-2026-01-39-02.cpp
@@ -0,0 +1,13 @@
+            for(int i =0; i<s.length(); i++){
+                m1[s[i]]++;
+                m2[t[i]]++;
+            }
+            int m1[256] = {0};
+            int m2[256] = {0};
+                return false;
+                
+            }
+            for(int i=0; i<256; i++){
+                if(m1[i] != m2[i]){
+                    return false;
+            }
