d8e5eb9e9a3be83d9f43c7626b1dfa4bc9101ff7
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-18 01:01:20 +0530

    0205-isomorphic-strings .cpp Hash Table Easy

---
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings-08-18-2026-01-01-19.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int m1[256]= {0};
+        int m2[256] = {0};
+        for(int i = 0; i< s.length(); i++){
+            if(m1[s[i]] != 0 && m1[s[i]] != t[i]+1 ){
+                return false;
+            }
+            if(m2[t[i]]!= 0 && m2[t[i]] != s[i]+1){
+                return false;
+            }
+            m1[s[i]]= t[i]+1;
+            m2[t[i]]= s[i]+1;
+        }
+        return true;
+    }
+};
