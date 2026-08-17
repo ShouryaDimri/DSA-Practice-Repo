976d195f298b7389e3576a23964f67fa4ad598bb
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-18 01:25:03 +0530

    rotate-string C++ DSA Easy

---
+++ takeUforward/Easy/rotate-string/rotate-string-08-18-2026-01-25-03.cpp
@@ -0,0 +1,13 @@
+            if(s.length() != goal.length()){
+                return false;
+            }
+            for (int i =0; i< s.length(); i++){
+                string rotated = s.substr(i) + s.substr(0,i);
+                if( rotated == goal){
+                    return true;
+                }
+        bool rotateString(string& s,string& goal){
+            //your code goes here
+            }
+            return false;
+        }
