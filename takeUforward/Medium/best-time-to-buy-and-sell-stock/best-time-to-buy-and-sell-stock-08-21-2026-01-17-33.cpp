29ae5dd45ec0e018829d81d6bcf298830dbaa7ff
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-21 01:17:33 +0530

    best-time-to-buy-and-sell-stock C++ DSA Medium

---
+++ takeUforward/Medium/best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock-08-21-2026-01-17-33.cpp
@@ -0,0 +1,12 @@
+                }
+                else{
+                if(arr[j]<arr[i]){
+                    break;
+                int diff;
+        for (int i =0; i< arr.size()-1; i++){
+            for(int j=i+1; j<arr.size(); j++){
+        int maxdiff=0;
+class Solution{
+public:
+    int stockBuySell(vector<int> arr, int n){
+                    diff= arr[j]-arr[i];
