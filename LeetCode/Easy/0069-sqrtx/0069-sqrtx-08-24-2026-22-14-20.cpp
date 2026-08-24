7743f29d82c7ef548912e25c6504a10a4fee27d5
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-24 22:14:20 +0530

    0069-sqrtx .cpp Math Easy

---
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx-08-24-2026-22-14-20.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low =0;
+        int high = x;
+        while(low<=high){
+            int mid = (low+high)/2;
+            long long val = 1LL* mid*mid;
+
+            if( val == x){
+                return mid;
+            }
+            else if (val <x){
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        return high;
+    }
+};
