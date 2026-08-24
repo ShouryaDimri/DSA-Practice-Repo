434095f48a078c8991174fca5283c7a3166fbd0d
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-24 20:55:26 +0530

    find-square-root-of-a-number C++ DSA Medium

---
+++ takeUforward/Medium/find-square-root-of-a-number/find-square-root-of-a-number-08-24-2026-20-55-25.cpp
@@ -0,0 +1,12 @@
+        if(val > n){
+        long long val = 1LL*mid*mid;
+
+      int high = n;
+      while(low<=high){
+        int mid= (low + high)/2;
+    int floorSqrt(int n)  {
+      int low = 0;
+class Solution {
+public:
+        //1LL
+        //1LL forces the multiplication to happen using long long.
