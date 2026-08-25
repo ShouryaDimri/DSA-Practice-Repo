e0abfb8954b89e2bed3c6d045e8ed9c88ac3f7fa
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-25 21:13:42 +0530

    koko-eating-bananas C++ DSA Medium

---
+++ takeUforward/Medium/koko-eating-bananas/koko-eating-bananas-08-25-2026-21-13-43.cpp
@@ -0,0 +1,14 @@
+            for (int i = 0; i < n; i++) {
+
+            long long totalhour = 0;
+            // find total hours needed at rate mid
+
+            int mid = (low + high) / 2;
+
+        while (low <= high) {
+
+        int high = temp[n - 1];
+        // maximum possible rate is the largest pile
+
+        int low = 1;
+        // minimum possible rate is 1
