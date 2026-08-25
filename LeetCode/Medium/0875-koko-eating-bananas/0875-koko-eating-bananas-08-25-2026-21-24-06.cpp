a990669f8846b6fdeabbe94aee99336c946371f2
Shourya Dimri <shouryadimricareer@gmail.com>
2026-08-25 21:24:06 +0530

    0875-koko-eating-bananas .cpp Array Medium

---
+++ LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas-08-25-2026-21-24-06.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n= piles.size();
+        int low = 1;
+        int high =*max_element(piles.begin(), piles.end());
+        while(low <= high){
+            int mid = (low + high)/2;
+            double totalhours =0;
+            for(int i =0; i<n; i++){
+                totalhours += (piles[i] + mid -1)/mid;
+            }
+            if(totalhours <= h){
+                high = mid -1;
+            }
+            else {
+                low = mid +1;
+            }
+        }
+        return low;
+    }
+};
