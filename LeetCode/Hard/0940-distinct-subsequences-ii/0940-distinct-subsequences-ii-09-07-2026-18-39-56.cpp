593df8ff87a5d175ce9f4559f52f92968e712926
Shourya Dimri <shouryadimricareer@gmail.com>
2026-09-07 18:39:57 +0530

    0940-distinct-subsequences-ii .cpp String Hard

---
+++ LeetCode/Hard/0940-distinct-subsequences-ii/0940-distinct-subsequences-ii-09-07-2026-18-39-56.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+
+        const int MOD = 1e9 + 7;
+
+        vector<long long> end(26, 0);
+
+        long long total = 0;
+
+        for (char c : s) {
+
+            int x = c - 'a';
+
+            long long add = (total + 1) % MOD;
+
+            total = (total - end[x] + add + MOD) % MOD;
+
+            end[x] = add;
+        }
+
+        return total;
+    }
+};
