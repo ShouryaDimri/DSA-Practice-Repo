af28f08bbb7c056ffdcc420ec14c8ddd9511aece
Shourya Dimri <shouryadimricareer@gmail.com>
2026-09-08 17:44:23 +0530

    3870-count-commas-in-range .cpp Math Easy

---
+++ LeetCode/Easy/3870-count-commas-in-range/3870-count-commas-in-range-09-08-2026-17-44-23.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int count =0;
+        for(int i=1; i<=n; ++i){
+            if(i>999){
+                count ++;
+            }
+            if (i >= 1000000)
+                count++;
+            
+            if (i >= 1000000000)
+                count++;
+        }
+        return count;
+    }
+};
